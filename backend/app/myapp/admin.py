0151a84d5c271e24a00a8ba80b505b0e5150ebab
immunity39 <so_kaicyo@icloud.com>
2024-01-28 16:55:33 +0900

    immunity create database & test data

--- backend/app/myapp/admin.py
+++ backend/app/myapp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import ProductInfo, ProductPilgrimage
+
+admin.site.register(ProductInfo)
+admin.site.register(ProductPilgrimage)

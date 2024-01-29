ffa0e7a644abcc26ecf5894020124d87ebf7d3b5
immunity39 <so_kaicyo@icloud.com>
2024-01-30 04:53:28 +0900

    view update

--- backend/app/myapp/views.py
+++ backend/app/myapp/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from rest_framework import viewsets
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from django.http import HttpResponse, JsonResponse
 from .models import ProductInfo, ProductPilgrimage
 from .serializers import ProductInfoSerializer, ProductPilgrimageSerializer

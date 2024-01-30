caa5b14e23c5cf8f56323ba3cab6590fa62e5f3b
immunity39 <so_kaicyo@icloud.com>
2024-01-30 19:32:27 +0900

    from test 1

--- backend/app/myapp/views.py
+++ backend/app/myapp/views.py
@@ -5,6 +5,8 @@ from rest_framework.response import Response
 from django.http import HttpResponse, JsonResponse
 from .models import ProductInfo, ProductPilgrimage
 from .serializers import ProductInfoSerializer, ProductPilgrimageSerializer
+import json
+from .serializers import PostProductPilgrimSerializer
 
 # Create your views here.
 def index(request):
@@ -24,3 +26,10 @@ def get_pilgrim_by_id(request, id):
         queryset = ProductPilgrimage.objects.filter(info_id=id)
         serializer_class = ProductPilgrimageSerializer(queryset, many=True)
         return JsonResponse(serializer_class.data, safe=False)
+
+@api_view(['POST'])
+def json_post_test(request):
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        serializer = PostProductPilgrimSerializer(data=data)
+        return JsonResponse(serializer.data, safe=False) 

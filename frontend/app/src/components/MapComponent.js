8f40e35abdfe5b631075d5f983cd24a7801795a3
koichi <satoyskkh200426@gmail.com>
2024-01-31 02:52:16 +0900

    Only the map is not displayed
    Only the map is not displayed

--- frontend/app/src/components/MapComponent.js
+++ frontend/app/src/components/MapComponent.js
@@ -1,77 +1,82 @@
 import React, { useEffect, useState, useRef } from 'react';
+import PropTypes from 'prop-types';
 import mapboxgl from 'mapbox-gl';
 import MapboxLanguage from '@mapbox/mapbox-gl-language';
 import 'mapbox-gl/dist/mapbox-gl.css';
+import token from './../.gitignore/accessToken';
+const MapComponent = ({ json_data, selectedSeriesId }) => {
+  mapboxgl.accessToken = token;
+  const mapContainer = useRef(null);
+  const [map, setMap] = useState(null);
+  const markers = useRef([]);
 
+  // 巡礼済みのピンの色
+  const visitedPinColor = '#FFA500';
 
+  useEffect(() => {
+    const initializeMap = () => {
+      const map = new mapboxgl.Map({
+        container: mapContainer.current,
+        center: [139.7670516, 35.6811673], // 東京駅を初期値点として表示
+        zoom: 15,
+        style: 'mapbox://styles/mapbox/streets-v12',
+      });
 
-export default function MapComponent({ json_data, selectedSeriesId }) {
-  // mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN ?? '';
-  // const mapContainer = useRef<HTMLDivElement | null>(null);
-  // const [map, setMap] = useState<mapboxgl.Map | null>(null);
-  // const markers = useRef([]); // 型注釈を削除
-  
-  // // 巡礼済みのピンの色
-  // const visitedPinColor = '#FFA500';
+      const language = new MapboxLanguage({ defaultLanguage: 'ja' });
+      map.addControl(language);
 
-  // useEffect(() => {
-  //   const initializeMap = () => {
-  //     const map = new mapboxgl.Map({
-  //       container: mapContainer.current,
-  //       center: [139.7670516, 35.6811673], // 東京駅を初期値点として表示
-  //       zoom: 15,
-  //       style: 'mapbox://styles/mapbox/streets-v12',
-  //     });
+      map.on('load', () => {
+        setMap(map);
+        map.resize();
+      });
+    };
 
-  //     const language = new MapboxLanguage({ defaultLanguage: 'ja' });
-  //     map.addControl(language);
+    if (!map) initializeMap();
+  }, [map]);
 
-  //     map.on('load', () => {
-  //       setMap(map);
-  //       map.resize();
-  //     });
-  //   };
+  useEffect(() => {
+    if (map && json_data) {
+      // 既存のマーカーをクリア
+      markers.current.forEach(marker => marker.remove());
+      markers.current = [];
 
-  //   if (!map) initializeMap();
-  // }, [map]);
-
-  // useEffect(() => {
-  //   if (map && json_data) {
-  //     // 既存のマーカーをクリア
-  //     markers.current.forEach(marker => marker.remove());
-  //     markers.current = [];
-
-  //     // selectedSeriesIdが9999の場合、全シリーズのロケーションを表示
-  //     if (selectedSeriesId === 9999) {
-  //       Object.values(json_data).flat().forEach(series => {
-  //         Object.values(series.address).forEach(location => {
-  //           const markerColor = location.gone === 1 ? visitedPinColor : 'red';
-  //           const marker = new mapboxgl.Marker({ color: markerColor })
-  //             .setLngLat([location.longitude, location.latitude])
-  //             .addTo(map);
-  //           markers.current.push(marker);
-  //         });
-  //       });
-  //     } else {
-  //       // 特定のシリーズのロケーションを表示
-  //       const series = Object.values(json_data).flat().find(s => s.id === selectedSeriesId);
-  //       if (series) {
-  //         Object.values(series.address).forEach(location => {
-  //           const markerColor = location.gone === 1 ? visitedPinColor : 'red';
-  //           const marker = new mapboxgl.Marker({ color: markerColor })
-  //             .setLngLat([location.longitude, location.latitude])
-  //             .addTo(map);
-  //           markers.current.push(marker);
-  //         });
-  //       }
-  //     }
-  //   }
-  // }, [map, json_data, selectedSeriesId]);
+      // selectedSeriesIdが9999の場合、全シリーズのロケーションを表示
+      if (selectedSeriesId === 9999) {
+        Object.values(json_data).flat().forEach(series => {
+          Object.values(series.address).forEach(location => {
+            const markerColor = location.gone === 1 ? visitedPinColor : 'red';
+            const marker = new mapboxgl.Marker({ color: markerColor })
+              .setLngLat([location.longitude, location.latitude])
+              .addTo(map);
+            markers.current.push(marker);
+          });
+        });
+      } else {
+        // 特定のシリーズのロケーションを表示
+        const series = Object.values(json_data).flat().find(s => s.id === selectedSeriesId);
+        if (series) {
+          Object.values(series.address).forEach(location => {
+            const markerColor = location.gone === 1 ? visitedPinColor : 'red';
+            const marker = new mapboxgl.Marker({ color: markerColor })
+              .setLngLat([location.longitude, location.latitude])
+              .addTo(map);
+            markers.current.push(marker);
+          });
+        }
+      }
+    }
+  }, [map, json_data, selectedSeriesId]);
 
   return (
     <div>
-      {/* <div ref={mapContainer} style={{ width: '100%', height: '100vh' }} /> */}
-      fff
+      <div ref={mapContainer} style={{ width: '100%', height: '100vh' }} />
     </div>
   );
-}
+};
+
+MapComponent.propTypes = {
+  json_data: PropTypes.object.isRequired,
+  selectedSeriesId: PropTypes.number
+};
+
+export default MapComponent;
